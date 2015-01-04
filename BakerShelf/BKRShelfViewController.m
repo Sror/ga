e743e1ab34c13766bdbaafae7bf6a432919df2d0
enzo <enzonieri@gmail.com>
2015-01-04 20:49:25 +0200

    Added bud icon png assets

--- BakerShelf/BKRShelfViewController.m
+++ BakerShelf/BKRShelfViewController.m
@@ -176,10 +176,6 @@
                                  target:self
                                  action:@selector(handleSubscribeButtonPressed:)];
         
-//        [self.subscribeButton setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-//                                                      [UIFont fontWithName:@"budicon" size:28.0],NSFontAttributeName,
-//                                                      [UIColor blackColor], NSForegroundColorAttributeName,nil]
-//                                            forState:UIControlStateNormal];
         
         self.categoryItem = [[BKRCategoryFilterItem alloc] initWithCategories:issuesManager.categories delegate:self];
 
