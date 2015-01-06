58c685480c5bbc7f2929f0f7578bcf502805f640
enzo <enzonieri@gmail.com>
2015-01-06 20:38:10 +0200

    Add SideBar

---
+++ BakerView/ReaderMainSidebar.h
@@ -0,0 +1,13 @@
+//
+//  ReaderMainSidebar.h
+//  General Aviation
+//
+//  Created by Enzo Nieri on 1/6/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReaderMainSidebar : UIScrollView
+
+@end
---
+++ BakerView/ReaderMainSidebar.m
@@ -0,0 +1,21 @@
+//
+//  ReaderMainSidebar.m
+//  General Aviation
+//
+//  Created by Enzo Nieri on 1/6/15.
+//
+//
+
+#import "ReaderMainSidebar.h"
+
+@implementation ReaderMainSidebar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
