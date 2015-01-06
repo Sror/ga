6f7732207fe650ec5a6692f00bd03bb86e39c8e4
enzo <enzonieri@gmail.com>
2015-01-06 21:06:35 +0200

    Add thubssideviewcontroller

---
+++ BakerView/ThumbsSideViewController.m
@@ -0,0 +1,21 @@
+//
+//  ThumbsSideViewController.m
+//  General Aviation
+//
+//  Created by Enzo Nieri on 1/6/15.
+//
+//
+
+#import "ThumbsSideViewController.h"
+
+@implementation ThumbsSideViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
